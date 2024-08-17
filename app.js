e89652f3355e4f2ef0369a3971852beba56231a2
Duy Thai <16250803+duyhthai@users.noreply.github.com>
2024-08-17 22:25:04 +0700

    Update app.js
    added more text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello CodePipeline!'));
+app.get('/', (req, res) => res.send('<h1>Hello CodePipeline!</h1><p>New line.</p>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
