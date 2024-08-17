54eeefce8c3259419b5fda12d3599eeeff0f63f6
Duy Thai <16250803+duyhthai@users.noreply.github.com>
2024-08-17 22:42:35 +0700

    Update app.js
    removed "Hello"

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('<h1>Hello CodePipeline!</h1><p>New line.</p>'));
+app.get('/', (req, res) => res.send('<h1>CodePipeline!</h1><p>New line.</p>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
